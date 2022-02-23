069cfbc98902a05c99cfc96034437844c1606188
Oier Mentxaka <oiermentxaka@gmail.com>
2022-02-23 10:49:22 +0100

    20220223

---
+++ RedesNeuronales/perceptron.py
@@ -0,0 +1,16 @@
+
+
+from ast import Not
+
+
+def funcion(X1,peso):
+    valor = X1*peso
+    pass
+
+def perceptron(X1,Z1):
+    peso = 0
+    salida = funcion(X1*peso)
+    tasaAprendizaje = 0.3
+    if salida != Z1 :
+        error = Z1 - salida
+        peso = tasaAprendizaje * error + peso
