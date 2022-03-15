aedddaf9520f52963970401f4b18e97c829d1938
Oier Mentxaka <oiermentxaka@gmail.com>
2022-03-15 09:59:58 +0100

    20220315

--- KaggleCoches/matriculasImagenes.py
+++ KaggleCoches/matriculasImagenes.py
@@ -4,7 +4,6 @@
 !git clone https://github.com/ultralytics/yolov5
 !pip install -r /content/yolov5/requirements.txt
 '''
-
 import torch
 import numpy
 import cv2
@@ -14,108 +13,111 @@ import glob
 from leer_texto import prepareReadEasy, readEasy
 import shutil
 
-#path = "/Users/mentxaka/KaggleCoches/coches/Españoles/*" #path de Oier
-path = "KaggleCoches\\coches\\Españoles\\*" #path de Eneko
+def loadModel(yoloPath):
 
-#yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
-yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
+    weightsPath = "KaggleCoches"+os.path.sep+"weights"+os.path.sep+"best-3.pt" #path de los pesos de Oier
 
-#weightsPath = '/Users/mentxaka/KaggleCoches/weights/best-3.pt' #path de los pesos de Oier
-weightsPath = r'C:\Users\eneko\GitHub\TK-VisionArtificial2\KaggleCoches\weights\best-3.pt' #path de los pesos de Eneko
+    # Model load 
+    model = torch.hub.load(yoloPath, 'custom', path=weightsPath, source='local')  # default
+    reader = prepareReadEasy()
 
-# Model load 
-model = torch.hub.load(yoloPath, 'custom', path=weightsPath, source='local')  # default
-reader = prepareReadEasy()
+    dst_path = "KaggleCoches" + os.path.sep + "results" + os.path.sep #path de Oier
 
-#folder reading
-imagefiles = glob.glob(path)
-imagefiles.sort()
+    #crear carpeta
+    if not os.path.isdir(dst_path):
+        os.mkdir(dst_path)
+        os.mkdir(dst_path+os.path.sep+"frames")
+        os.mkdir(dst_path+os.path.sep+"crops")
 
-print(path)
-print(imagefiles)
-
-coordenadas = []
-for image in imagefiles:
     
-    # Inference
-    results = model(image)
-    # Results
-    print(results.pandas().xyxy[0])
-    x0, y0, x1, y1, _, _ = results.xyxy[0][0].numpy().astype(int)
-    
-    x00,y00,x11,y11 = int(x0),int(y0), int(x1), int(y1)
+    return model, reader
 
-    print(type(x0))
-    cropped_image = image[x00:x11, y00:y11]
+def video2Frames(path):
+    print("Lectura del video comenzadad")
 
-    cv2.imwrite("cropped_img.jpg", cropped_image)
-    break
-
-    coordenadas.append(coor)
-    
-#coordenadas de las matriculas de todas las imagenes
-print(coordenadas)
-#path = "/Users/mentxaka/KaggleCoches/runs/detect/exp*" #path de detecciones de Oier
-path = "KaggleCoches\\runs\\detect\\exp*" #path de detecciones de Eneko
-
-#folder reading
-print(path)
-carpetas = glob.glob(path)
-carpetas.sort()
-
-print("CARPETAS")
-print(carpetas)
-lectura = ""
-
-#dst_path = "/Users/mentxaka/"+"KaggleCoches" + os.path.sep + "results" + os.path.sep #path de Oier
-dst_path = "KaggleCoches\\results\\" #path de Eneko
-
-#crear carpeta
-if not os.path.isdir(dst_path):
-    os.mkdir(dst_path)
-    print("Carpeta de salida {} creada".format(dst_path))
-
-for path in carpetas:
-    #pathCrops = path + os.path.sep + "crops" + os.path.sep + "plate"+os.path.sep+"*"
-    pathCrops = path + "\\crops\\plate\\*" 
-
-    print("path = "+path)
-    print("pathCrops = "+pathCrops)
+    pathVideoFrames = "KaggleCoches"+os.path.sep+"results"+os.path.sep+"frames"+os.path.sep 
+    cap = cv2.VideoCapture(path)
+    # # Read until video is completed
+    cont = 0
+    while(cap.isOpened()):
+    # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            if cont % 5 == 0:
+                cv2.imwrite(pathVideoFrames+str(cont)+".jpeg", frame)
+        else: 
+            break
+        cont = cont+1
 
 
-    imagefiles = glob.glob(pathCrops)
-    imagefiles.sort() 
-
-
-    print(imagefiles)
-
-
-    for image in imagefiles:
-        lectura += "nombre foto " + image.split(os.path.sep)[-1] + "\n"
-        #lectura de matricula
-        for text in readEasy(reader, image):
-            lectura += text[1]
+    print("Lectura del video completada")
         
-        lectura += "\n"
 
-        print(lectura)
+def folderReading(path, model):
+    print("Lectura de carpeta comenzadad")
 
-        #mover carpeta
-        shutil.move(image, dst_path)
-
-    try:
-        os.remove(path)
-    except OSError as e:
-        print("Error: %s : %s" % (path, e.strerror))
-
-#pathText = "/Users/mentxaka/"+"KaggleCoches" + os.path.sep + "results" + os.path.sep + "log.txt" #path text de Oier
-pathText = r"C:\Users\eneko\GitHub\TK-VisionArtificial2\KaggleCoches" + os.path.sep + "results" + os.path.sep + "log.txt" #path text de Eneko
-
-with open(pathText, 'w') as f:
-    f.write(lectura)
+    #folder reading
+    imagefiles = glob.glob(path)
+    imagefiles.sort()
 
 
-    
+    coordenadas = []
+    for image in imagefiles:
+        print(image)
+        try:
+            # Inference
+            results = model(image)
+            # Results
+            x0, y0, x1, y1, _, _ = results.xyxy[0][0].numpy().astype(int)        
+            x00,y00,x11,y11 = int(x0),int(y0), int(x1), int(y1)
+            imageOCV = cv2.imread(image)
+            cropped_image = imageOCV[y00:y11, x00:x11] 
+            cv2.imwrite("KaggleCoches"+os.path.sep+"results"+os.path.sep+"crops"+os.path.sep+image.split(os.path.sep)[-1], cropped_image)
+        
+        except:
+            print("la imagen "+image.split(os.path.sep)[-1]+" no tiene matrículas reconocibles")
+        
+    print("Lectura de carpeta completada")
 
 
+def textReading(path,reader):   
+    print("Lectura del OCR comenzadad")
 
+    #folder reading
+    results = glob.glob(path)
+    results.sort()
+    lectura = ""
+
+    for image in results:
+        try:
+            lectura += "nombre foto " + image.split(os.path.sep)[-1] + "\n"
+            #lectura de matricula
+            for text in readEasy(reader, image):
+                lectura += text[1]
+            
+            lectura += "\n"
+            pathText = "KaggleCoches" + os.path.sep + "results" + os.path.sep + "log.txt" #path text de Oier
+        except:
+            print("la imagen "+image.split(os.path.sep)[-1]+" no tiene matrículas legibless")
+    print("Lectura del OCR completada")
+
+    with open(pathText, 'w') as f:
+        f.write(lectura)
+
+
+yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
+#yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
+model, reader = loadModel(yoloPath)
+
+# #IMAGENES        
+# path = "KaggleCoches"+os.path.sep+"coches"+os.path.sep+"Españoles"+os.path.sep+"*" 
+# path = "KaggleCoches"+os.path.sep+"coches"+os.path.sep+"Españoles"+os.path.sep+"*"
+
+# #VIDEO
+# path = "KaggleCoches"+os.path.sep+"coches"+os.path.sep+"video.MOV" 
+# video2Frames(path)
+#path = "KaggleCoches"+os.path.sep+"results"+os.path.sep+"frames"+os.path.sep+"*"
+
+#folderReading(path, model)
+path = "KaggleCoches"+os.path.sep+"results"+os.path.sep+"crops"+os.path.sep+"*" 
+textReading(path,reader)
