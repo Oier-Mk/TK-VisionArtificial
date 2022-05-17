e3ed7201abd2649adf49f2c58a55fc26a45e8c46
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2022-05-17 08:36:16 +0200

    Web changes

--- PedestrianDetector/main.py
+++ PedestrianDetector/main.py
@@ -1,8 +1,8 @@
 from startYolo import loadYolo
 
 #yoloPath = '/content/yolov5' #path yolo de collab
-yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
-#yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
+#yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
+yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
 modeloYolo = loadYolo(yoloPath)
 
     
