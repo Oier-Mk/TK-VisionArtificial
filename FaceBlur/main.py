5704e2cb9b02bff7735e2a0e174e2484f5c6ef7d
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2022-05-25 10:01:42 +0200

    funciona con rectángulos (cambiar a circulo)

--- FaceBlur/main.py
+++ FaceBlur/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 import os
 import torch
+import traceback
 
 relative = os.getcwd()
 
@@ -35,52 +36,49 @@ def getNDetections(results):
 
 if __name__ == '__main__' :
 
-    yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
-    #yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
+    #yoloPath = '/Users/mentxaka/yolov5' #path yolo de Oier
+    yoloPath = r"C:\Users\eneko\yolov5" #path yolo de Eneko
 
     modelo = loadYolo(yoloPath)
 
-    path = "/Users/mentxaka/Documents/Y - Trabajo/TK - VisionArtificial/FaceBlur/images/People.jpeg"
+    path = r"C:\Users\eneko\GitHub\TK-VisionArtificial\FaceBlur\images\People.jpeg"
     
-    #img, r = selectRange(path)
+    img, r = selectRange(path)
 
-    # xRange1 = int(r[0])
-    # yRange1 = int(r[1])  
-    # xRange2 = int(r[2])
-    # yRange2 = int(r[3])
+    xRange1 = int(r[0])
+    yRange1 = int(r[1])  
+    xRange2 = int(r[2])
+    yRange2 = int(r[3])
 
-    # pRange1 = (xRange1,yRange1)
-    # pRange2 = (xRange2,yRange2)
+    pRange1 = (xRange1,yRange1)
+    pRange2 = (xRange2,yRange2)
 
     img = cv2.imread(path)
 
-    #TODO tenemos que usar los puntos de arriba para ver la deteccion
-
-    # Inference
     results = modelo(img)
-    results.save()
-
+    
+    i = 0
+    while True:
+        try:
+            print(i)
+            x0, y0, x1, y1, _, _ = results.xyxy[0][i].numpy().astype(int)
+            x00,y00,x11,y11 = int(x0),int(y0), int(x1), int(y1)
+            #TODO checkear esta condicion, no se porque no se cumple
+            if(xRange1<x00 & xRange2>x11 & yRange1 < y00 & yRange2>y11):
+                cv2.rectangle(img, (x00, y00), (x11, y11), (0, 0, 0),-1, 8)
+            i+=1
+            print("One face blured")
+        except Exception:
+            print(traceback.format_exc())
+            print("No more faces detected")
+            break
     # Results
 
-    #for i in range(getNDetections(results)):
-    for i in range(1):
-        x0, y0, x1, y1, _, _ = results.xyxy[0][i].numpy().astype(int)  
-        print(x0, y0, x1, y1, _, _ )      
-        x00,y00,x11,y11 = int(x0),int(y0), int(x1), int(y1)
-
-        w = x11-x00
-        h = y11-y00
-
-        mask = np.zeros(img.shape, dtype='uint8')
-        # blur(mask, w, h, x00, y00, x11, y11)
-        centro = (int(x00+(x11/2)),int(y00+(y11/2)))
-        radio = int((math.sqrt(w * w + h * h) // 2)/2)
-        cv2.circle(mask, centro, radio, (255, 255, 255), -1)
-        pixelada = np.where(mask > 0, cv2.medianBlur(img, 99), img)
+    
 
 
     # Display cropped image
-    cv2.imshow(path.split(os.path.sep)[-1], pixelada)
+    cv2.imshow(path.split(os.path.sep)[-1], img)
     cv2.waitKey(0)
 
     
