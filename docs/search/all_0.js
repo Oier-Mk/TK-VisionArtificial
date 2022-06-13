64159d4eb34f1bc19fb8fad882f638982b1d9cac
Oier Mentxaka <oiermentxaka@gmail.com>
2022-06-13 11:42:31 +0200

    adding documentation

---
+++ docs/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['api_0',['api',['../namespaceapi.html',1,'']]],
+  ['api_2epy_1',['api.py',['../api_8py.html',1,'']]],
+  ['app_2',['app',['../namespaceapi.html#afe63fea7be31b0200b496d08bc6b517d',1,'api']]]
+];
---
+++ docs/search/variables_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['templates_0',['templates',['../namespaceapi.html#aff4ff3a7bf99e704dc5f9eb72480786a',1,'api']]]
+];
