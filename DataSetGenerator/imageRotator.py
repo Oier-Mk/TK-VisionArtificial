c763b6765648879782c1062540998e233016bb2f
Oier Mentxaka <oiermentxaka@gmail.com>
2022-10-07 17:57:29 +0200

    tries

--- DataSetGenerator/imageRotator.py
+++ DataSetGenerator/imageRotator.py
@@ -5,6 +5,9 @@ import cv2
 from PIL import Image 
 import math
 
+def round(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 
 def rotatebox(rect, center, degrees):
     rads = math.radians(degrees)
@@ -28,27 +31,70 @@ def rotateImage(image, angle):
 
   return result
 
-def annotations2points(path, w, h, angle):
+def points2yolo(points,size):
 
+    width = size[0]
+    height = size[1]
+    xmin = points[0][0]
+    ymin = points[0][1]
+    xmax = points[2][0]
+    ymax = points[2][1]
+
+    puntoX = abs(xmin-xmax)/2 + xmin
+    puntoY = abs(ymin-ymax)/2 + ymin
+    ancho = abs(xmin-xmax)
+    alto = abs(ymin-ymax)
+
+    propX = puntoX/width
+    propY = puntoY/height
+    propAncho = ancho/width
+    propAlto = alto/height
+
+    #print("0 " + str(propX) + " " + str(propY) + " " + str(propAncho) + " " + str(propAlto))
+    #print("0 {:.6f} {:.6f} {:.6f} {:.6f}\n".format(propX, propY, propAncho, propAlto))
+
+    print(width,height)
+    print("0 {:.6f} {:.6f} {:.6f} {:.6f}".format(propX, propY, propAncho, propAlto))
+    print(puntoX)
+    print(puntoY)
+    print(ancho)
+    print(alto)
+    return([(xmin,ymin),(xmax,ymin),(xmax,ymax),(xmin,ymax)])
+
+def yolo2points(values,size):
+
+    values = values[:-1]
+    values = values.split(" ")
+    values[0] = float(values[0])
+    values[1] = float(values[1])
+    values[2] = float(values[2])
+    values[3] = float(values[3])
+    values[4] = float(values[4])
+
+    width = size[0]
+    height = size[1]
+
+    propX = values[1]
+    propY = values[2]
+    propAncho = values[3]
+    propAlto = values[4]
+
+    puntoX = width * propX
+    puntoY = height * propY
+    ancho = width * propAncho
+    alto = height * propAlto
+
+    puntos = [(puntoX-ancho/2,puntoY-alto/2),(puntoX+ancho/2,puntoY-alto/2),(puntoX+ancho/2,puntoY+alto/2),(puntoX-ancho/2,puntoY+alto/2)]
+    return(puntos)
+
+def annotations2points(path, w, h, angle):
     file = open(path, 'r')
     Lines = file.readlines()
     data = []
     for l in Lines:
-        l = l[:-1]
-        values = l.split(" ")
-        centerX = w * float(values[1])
-        centerY = h * float(values[2])
-        relW = float(values[3]) * w
-        relH = float(values[4]) * h
-        puntos = [[centerX-relW,centerY-relH],[centerX+relW,centerY-relH],[centerX+relW,centerY+relH],[centerX-relW,centerY+relH]]
-        print(w,h)
-        print(values)
-        print(centerX)
-        print(centerY)
-        print(relW)
-        print(relH)
-        print(puntos)
-        print("---------------")
+        puntos = yolo2points(l,(w,h))
+        centerX = puntos[0][0] - puntos[2][0]
+        centerY = puntos[0][1] - puntos[2][1]
         data.append(rotatebox(puntos,(centerX,centerY),angle))
     return data 
 
@@ -66,11 +112,8 @@ def rotator(pathIMG, pathTXT, angle):
 
     annotations = annotations2points(pathTXT, w, h, angle)
 
-    for a in annotations:
+    for a in annotations:       
         print(a)
-        print(a[0])
-        print(a[2])
-        
         img = cv2.rectangle(img, a[0], a[2], (255, 0, 0), 1)
         
     return img
--- DataSetGenerator/prueba.py
+++ DataSetGenerator/prueba.py
@@ -1,8 +1,33 @@
+def yolo2points(values,size):
+
+    values = values[:-1]
+    values = values.split(" ")
+    values[0] = float(values[0])
+    values[1] = float(values[1])
+    values[2] = float(values[2])
+    values[3] = float(values[3])
+    values[4] = float(values[4])
+
+    width = size[0]
+    height = size[1]
+
+    propX = values[1]
+    propY = values[2]
+    propAncho = values[3]
+    propAlto = values[4]
+
+    puntoX = width * propX
+    puntoY = height * propY
+    ancho = width * propAncho
+    alto = height * propAlto
+
+    puntos = [(puntoX-ancho/2,puntoY-alto/2),(puntoX+ancho/2,puntoY-alto/2),(puntoX+ancho/2,puntoY+alto/2),(puntoX-ancho/2,puntoY+alto/2)]
+    return(puntos)
+
+print(yolo2points("0 0.450000 0.217333 0.132000 0.242667\n",(500,375)))
 
 import math
-import matplotlib.pyplot as plt
-
-def rotatebox( rect, center, degrees ):
+def rotatebox(rect, center, degrees):
     rads = math.radians(degrees)
 
     newpts = []
@@ -11,35 +36,10 @@ def rotatebox( rect, center, degrees ):
         diag_x = center[0] - pts[0]
         diag_y = center[1] - pts[1]
 
-        # Rotate the diagonal from center to top left
-
         newdx = diag_x * math.cos(rads) - diag_y * math.sin(rads)
         newdy = diag_x * math.sin(rads) + diag_y * math.cos(rads)
         newpts.append( (center[0] + newdx, center[1] + newdy) )
 
     return newpts
 
-rect = [[50,50],[50,120],[150,120],[150,50]]
-
-rect = rotatebox( rect, (100,100), 135 )
-'''
-width = 6
-height = 6
-xmin = 2
-ymin = 2
-xmax = 4
-ymax = 4
-
-puntoX = abs(xmin-xmax)/2 + xmin
-puntoY = abs(ymin-ymax)/2 + ymin
-ancho = abs(xmin-xmax)
-alto = abs(ymin-ymax)
-
-propX = puntoX/width
-propY = puntoY/height
-propAncho = ancho/width
-propAlto = alto/height
-
-#print("0 " + str(propX) + " " + str(propY) + " " + str(propAncho) + " " + str(propAlto))
-print("0 {:.6f} {:.6f} {:.6f} {:.6f}\n".format(propX, propY, propAncho, propAlto))
-'''
+print(rotatebox([(2,2),(4,2),(4,4),(2,4)],(1,1),90))
