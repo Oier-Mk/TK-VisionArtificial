caddc2535d01df9586fb09bd66bf91289c3b57f9
Oier Mentxaka <oiermentxaka@gmail.com>
2022-10-11 17:31:44 +0200

    annotations updated

--- DataSetGenerator/checkAnnotations.py
+++ DataSetGenerator/checkAnnotations.py
@@ -1,21 +1,38 @@
 import os       
 
-directory = r"FaceDataset\YOLOannotations"
-directory2 = r"FaceDataset\dataset"
+YOLOannotations = "/Users/mentxaka/Github/TK-VisionArtificial/DataSetGenerator/FaceDataset/YOLOannotations"
+dataset = "/Users/mentxaka/Github/TK-VisionArtificial/DataSetGenerator/FaceDataset/dataset"
 
-print(directory)
-print(directory2)
-counter = 0
+print(YOLOannotations)
+print(dataset)
 
-for filename in os.listdir(directory):
-    filename = filename.split(".")[0] + ".jpg"
-    print(directory2+os.path.sep+filename)
-    if(not os.path.isfile(directory2+os.path.sep+filename)):
+def function1():
+    counter = 0
+    for filename in os.listdir(YOLOannotations):
+        print(str(os.path.exists(YOLOannotations+os.path.sep+filename)) + " file in annotations")
+        filename = filename.split(".")[0] + ".jpg"
+        print(str(os.path.exists(dataset+os.path.sep+filename)) + " file in photos")
+        print("---------------")
+        '''print(dataset+os.path.sep+filename)
+        if(not os.path.exists(dataset+os.path.sep+filename)):
+            counter += 1
+            print(filename)
+        '''
         counter += 1
-        print(filename)
-    
-print(counter)  
+    print(counter)  
 
 
-def deleteImg(name):
-    os.remove("FaceDataset\dataset\\"+name+".jpg" )
+
+def function2():
+    counter = 0
+    for filename in os.listdir(dataset): 
+        txtname = filename.split(".")[0] + ".txt"
+        imgname = filename.split(".")[0] + ".jpg"
+        if(not os.path.exists(YOLOannotations+os.path.sep+txtname)):
+            print("Eliminamos "+dataset+os.path.sep+imgname)
+            counter+=1
+            os.remove(dataset+os.path.sep+filename)
+    print(counter)
+
+function2()
+#function1()
