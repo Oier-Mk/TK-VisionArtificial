0f0ca86c402ffe43d94cd6da510e059d93e72b7e
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2022-11-14 20:22:08 +0100

    model testing

--- Face-PlateDetector/multiclass.py
+++ Face-PlateDetector/multiclass.py
@@ -1,23 +1,29 @@
 import FacePlateBlur.facePlateDetector as fpd
 import cv2
 import time
+import os 
+from os import listdir
 
 #yoloPath = '/Users/mentxaka/yolov5' 
-yoloPath = "/content/yolov5"
+#yoloPath = "/content/yolov5"
+yoloPath = r"C:\Users\eneko\yolov5"
 
 multiclassModel = fpd.loadYolo(yoloPath)
 
 #inPATH = "/Users/mentxaka/Github/TK-VisionArtificial/Face-PlateDetector/Arona_persona.jpg"
 #outPATH = "/Users/mentxaka/Github/TK-VisionArtificial/Face-PlateDetector/mod_Arona_persona.jpg"
 
-inPATH = "/content/TK-VisionArtificial/Face-PlateDetector/Arona_persona.jpg"
-outPATH = "/Users/mentxaka/Github/TK-VisionArtificial/Face-PlateDetector/mod_Arona_persona.jpg"
+inPATH = r"C:\Users\eneko\GitHub\TK-VisionArtificial\Face-PlateDetector\input_imgs"
+outPATH = r"C:\Users\eneko\GitHub\TK-VisionArtificial\Face-PlateDetector\output_imgs"
 
 start_time = time.time()*1000
 
-points = fpd.multiclassDetection(inPATH,multiclassModel)
-img = fpd.multiclassBoxing(cv2.imread(inPATH),points)
+for image in os.listdir(inPATH):
+    filename = inPATH + os.sep + image
+    points = fpd.multiclassDetection(filename,multiclassModel)
+    img = fpd.multiclassBoxing(cv2.imread(filename),points)
+    cv2.imwrite(outPATH + os.path.sep + image,img)
 
 print("MultiClass --- %s miliseconds ---" % str((time.time()*1000 - start_time)))
 
-cv2.imwrite(outPATH,img)
+
